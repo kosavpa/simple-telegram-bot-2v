5204d70ce2c352f695ce0b0421066c4d2747a9ba
kosavpa <kosavpa@yandex.ru>
2023-01-09 14:40:22 +0400

    фыв

---
+++ src/test/java/owl/home/simpletelegrambot/GeneralGatheringApplicationTests.java
@@ -0,0 +1,13 @@
+package owl.home.simpletelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralGatheringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
